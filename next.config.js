81f2f888bb492342475ffe679b57ed011297b598
jim wen <jim.wen@monash.edu>
2025-08-29 01:45:58 +1000

    Generate A Static Export & Set Up AssestPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
+
+  // Make all asset URLs include /dist so they work when popup is dist/index.html
+  assetPrefix: '/dist',
+  basePath: '/dist',
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
