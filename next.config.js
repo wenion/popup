65fadd823862d19536c1308cce157e70114589b4
Jim Wen <jimwendev@gmail.com>
2025-12-03 15:46:59 +1100

    Export build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
 
-  // // Optional: Change the output directory `out` -> `dist`
-  // distDir: "dist",
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
 
-  // // Make all asset URLs include /dist so they work when popup is dist/index.html
-  // assetPrefix: "/dist",
-  // basePath: "/dist",
-  // reactStrictMode: true,
-  // images: { unoptimized: true },
+  // Make all asset URLs include /dist so they work when popup is dist/index.html
+  assetPrefix: "/dist",
+  basePath: "/dist",
+  reactStrictMode: true,
+  images: { unoptimized: true },
   publicRuntimeConfig: {
     homepage: "http://127.0.0.1:3000/",
     loginPage: "http://127.0.0.1:3000/login",
